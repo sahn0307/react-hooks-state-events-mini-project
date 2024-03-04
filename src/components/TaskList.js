2c40da1339dee8982b4b0bad24a5b6d414a2c693
Sung-Jin Ahn <sahn0307@gmail.com>
2024-03-03 22:28:49 -0700

    add delete functionality

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Task from "./Task";
 
-function TaskList({ tasks }) {
+function TaskList({ tasks, onDelete }) {
   return (
     <div>
       {tasks.map((task, index) => (
-        <Task key={index} text={task.text} category={task.category} />
+        <Task key={index} text={task.text} category={task.category} onDelete={() => onDelete(task)} />
       ))}
     </div>
   );
