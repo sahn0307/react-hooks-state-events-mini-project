43975c2cb5afbcbd05d013780ee948d33d24ee91
Sung-Jin Ahn <sahn0307@gmail.com>
2024-03-03 22:14:43 -0700

    display tasks on the page

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,13 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS}/>
     </div>
   );
 }
